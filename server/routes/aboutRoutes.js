4bfddb6e7ed0e3a1b3fc1afecd3a64e5cafb089e
Daniel Lepcha <daniel@kali.google.com>
2025-12-02 15:51:45 +0545

    Remove server folder for frontend-only branch

--- server/routes/aboutRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { getAbout, updateAbout } from '../controllers/aboutController.js';
-
-const router = express.Router();
-
-router.get('/', getAbout);
-router.put('/', updateAbout);
-
-export default router;
