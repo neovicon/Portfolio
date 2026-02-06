c7728e915262adc9d5d327b65738e2a0ed4a37f3
neovicon <240396sarun@cosmoscollege.edu.np>
2026-02-06 22:07:47 +0545

    Delete server directory

--- server/routes/wallpaperRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { getWallpaper, updateWallpaper } from '../controllers/wallpaperController.js';
-
-const router = express.Router();
-
-router.get('/', getWallpaper);
-router.put('/', updateWallpaper);
-
-export default router;
